272a931892236fb97d0f885d9a678ae9624fac9c
Tryston Minsquero <tam190005@utdallas.edu>
2022-01-16 04:17:08 -0600

    Added 3D playability

---
+++ Assets/_Scripts/3D Display/PieceDisplay3D.cs
@@ -0,0 +1,40 @@
+using System;
+using _Scripts;
+using UnityEngine;
+
+public class PieceDisplay3D : PieceDisplay
+{
+    [SerializeField] private GameObject pieceTilePrefab;
+
+    private float _pieceDepth;
+
+    public override void Setup(GamePiece piece, Vector3 spawnPoint)
+    {
+        base.Setup(piece, spawnPoint);
+        _pieceDepth = pieceTilePrefab.transform.localScale.z;
+        transform.SetZ(transform.position.z - _pieceDepth);
+        Construct3DTiles();
+    }
+
+    protected override void UpdateLevel(int level)
+    {
+        transform.SetZ(-_pieceDepth * level - _pieceDepth);
+    }
+
+    private void Construct3DTiles()
+    {
+        for (int j = 0; j < _gamePiece.Height; j++)
+        {
+            for (int i = 0; i < _gamePiece.Width; i++)
+            {
+                if (_gamePiece.PieceMatrix[j, i])
+                {
+                    GameObject tile = Instantiate(pieceTilePrefab, transform);
+                    tile.GetComponent<MeshRenderer>().material.color = _gamePiece.Color;
+                    tile.transform.localPosition =
+                        new Vector3(i + .5f, -j - .5f, _pieceDepth / 2 + .001f);
+                }
+            }
+        }
+    }
+}
